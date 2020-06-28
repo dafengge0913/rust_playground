7abe57c32e58926dead10c318f8ecc394f81f5bd
dafengge0913 <dafengge0913@hotmail.com>
2020-06-28 14:59:52 +0800

    module

---
+++ restaurant/src/front_of_house/hosting.rs
@@ -0,0 +1,14 @@
+
+// pub mod hosting {
+//     pub fn add_to_waitlist() {}
+
+//     fn seat_at_table() {}
+// }
+
+pub fn add_to_waitlist() {}
+
+mod serving {
+    fn take_order() {}
+    fn serve_order() {}
+    fn take_payment() {}
+}
